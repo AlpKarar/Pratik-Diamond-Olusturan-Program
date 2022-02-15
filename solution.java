b04d91ae1fe63e83af87ff6f2d5b7df6937bf88c
Alp Karar <88462979+AlpKarar@users.noreply.github.com>
2022-02-15 16:55:20 +0300

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,41 @@
+/*
+  Ödev
+  Java'da döngüler kullanarak yıldızlar ile elmas yapınız.
+*/
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        int n;
+        
+        System.out.print("Enter an integer: ");
+        n = input.nextInt();
+        
+        for (int i=0; i < n; i++) {
+            for (int j=0; j < (n - 1 - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k=0; k < (2*i - 1); k++) {
+                System.out.print("*");
+            }
+            
+            System.out.println("");
+        }
+        for (int i=n-2; 0 <= i; i--) {
+            for (int j=0; j < (n - 1 - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k=0; k < (2*i - 1); k++) {
+                System.out.print("*");
+            }
+            
+            System.out.println("");
+        }
+        
+    }
+}
